4d586bc3f1cc3e2d0de3f93cb85d4cafaf1e4dbf
Dan Tracy <dant@aweber.com>
2015-06-05 00:36:34 -0400

    Rename idoneit to idonethis to match the service

--- tests.py
+++ tests.py
@@ -2,20 +2,18 @@ import os
 import unittest
 
 import betamax
-import idoneit
+import idonethis
 import requests
 
 try:
     from unittest import mock
-    from io import StringIO
 except ImportError:
     import mock
-    from cStringIO import StringIO
 
 
 # No requests should come from Travis
 RECORD_MODE = 'never' if os.environ.get('TRAVIS_GH3') else 'once'
-AUTH_TOKEN = 'Token ' + os.environ.get('IDONEIT_TOKEN', 'x' * 20)
+AUTH_TOKEN = 'Token ' + os.environ.get('idonethis_TOKEN', 'x' * 20)
 
 
 class BetamaxMixn(object):
@@ -30,7 +28,8 @@ class BetamaxMixn(object):
         with betamax.Betamax.configure() as config:
             config.cassette_library_dir = 'fixtures'
             config.default_cassette_options['record_mode'] = RECORD_MODE
-            config.define_cassette_placeholder('Token <AUTH_TOKEN>', AUTH_TOKEN)
+            config.define_cassette_placeholder('Token <AUTH_TOKEN>',
+                                               AUTH_TOKEN)
 
 
 class TestSubmitDone(BetamaxMixn, unittest.TestCase):
@@ -40,18 +39,18 @@ class TestSubmitDone(BetamaxMixn, unittest.TestCase):
 
         with self.assertRaises(ValueError):
             with self.vcr.use_cassette('bad_auth_token'):
-                idoneit.submit_done(self.session, 'team', 'done')
+                idonethis.submit_done(self.session, 'team', 'done')
 
         self.session.headers['Authorization'] = AUTH_TOKEN
 
     def test_using_the_wrong_team(self):
         with self.assertRaises(ValueError):
             with self.vcr.use_cassette('bad_team'):
-                idoneit.submit_done(self.session, 'team', 'done')
+                idonethis.submit_done(self.session, 'team', 'done')
 
     def test_a_good_request(self):
         with self.vcr.use_cassette('good_post_request'):
-            idoneit.submit_done(self.session, 'aweber-be-bof', 'I did it!')
+            idonethis.submit_done(self.session, 'aweber-be-bof', 'I did it!')
 
 
 class TestGrabText(unittest.TestCase):
@@ -60,8 +59,8 @@ class TestGrabText(unittest.TestCase):
     def test_stdin_available(self, stdin):
         stdin.isatty.return_value = False
         stdin.read.return_value = 'testing'
-        self.assertEqual('testing', idoneit.get_done_text())
+        self.assertEqual('testing', idonethis.get_done_text())
 
-    @mock.patch('idoneit.subprocess.call')
+    @mock.patch('idonethis.subprocess.call')
     def test_bringing_up_an_editor(self, subprocess_call):
-        self.assertEqual('Today I did...', idoneit.get_done_text())
+        self.assertEqual('Today I did... ', idonethis.get_done_text())
